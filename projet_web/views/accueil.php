43e6c98dc3aade075a55f2da12b19512551b679b
melanie-lancha <melanie.lancha@gmail.com>
2024-11-02 22:46:21 +0100

    mise en page

--- projet_web/views/accueil.php
+++ projet_web/views/accueil.php
@@ -6,35 +6,60 @@
         <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css">
         <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
         <link rel ="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="/assets/style.css">
         <script src="https://cdn.jsdelivr.net/npm/vue"></script>
     </head>
+
     <body>
-        <p> Hello World ! </p>
-        <button type="button" class="btn btn-primary">Ce bouton a un style</button>
-        <p> il est censé y avoir un alert </p>
+
+        <div id="titre"> 
+            <p><h1>Escape game géographique</h1></p>
+            <!-- <p> (il est censé y avoir un alert) </p> -->
+        </div>
+
+        <div id="map"></div>
         
-        <form id="app">
+        <div id="inventaire">
+            <p><u>INVENTAIRE</u></p>
+        </div>
+
+        <div id="identification">
+            <p><h2>S'identifier : </h2></p>
+            <form action="" method="post">
+                <!--<label>Nom<input type="text" name="prenom"></label> -->
+                <input type="text" name="prenom" placeholder="Nom">
+                <!--<label>Mot de passe<input type="password" name="password"></label>-->
+                <input type="password" name="password" placeholder="Mot de passe">
+                <input type="submit" name="envoi" value="OK">
+            </form>
+            
+        </div>      
+                        
+        
+       <!-- <form id="app">
             <input type="text" v-model="message">
             <p>{{message}}</p>
-        </form>
+        </form> -->
         
         <?php
-            $link = mysqli_connect('localhost', 'root', 'root','geobase');
+           // $link = mysqli_connect('localhost', 'root', 'root','geobase');
 
             // if (!$link) {
             // die('Erreur de connexion');
             // } else {
             // echo 'Succès... ';
             // };
-            $results = mysqli_query($link, "SELECT nom, insee FROM communes LIMIT 2");
-            foreach ($results as $result) {
+            // $results = mysqli_query($link, "SELECT nom, insee FROM communes LIMIT 2");
+           // foreach ($results as $result) {
                 // $result est un tableau associatif
-                echo '<h3>'.$result["nom"].' , '.$result["insee"].'</h3>';
-              };
+                //echo '<h3>'.$result["nom"].' , '.$result["insee"].'</h3>';
+              //};
         ?>
 
-        <div id="map"></div>
+        
 
         <script src="assets/javascript.js"></script>
     </body>
